eb9e8f186b2fb77709d2272bc23e1521816fc296
G <gorkang@gmail.com>
2023-07-07 15:14:37 +0100

    include pdf

--- _common.R
+++ _common.R
@@ -1,16 +1,33 @@
+# install.packages("formatR")
+
 set.seed(2023)
 
 library(ggplot2); ggplot2::theme_set(ggplot2::theme_minimal(10))
 
-
+# Chunks options
 knitr::opts_chunk$set(
   comment = "#>",
   collapse = TRUE,
-  fig.retina = 2,
+  # fig.retina = 2,
   # fig.width = 6,
-  fig.asp = 2/3,
+  # fig.asp = 2/3,
   fig.show = "hold"
+  # tidy = TRUE,
+  # tidy.opts=list(width.cutoff=60)
 )
+
+# Chunk options only for pdf output
+if (knitr::is_latex_output()) {
+  knitr::opts_chunk$set(
+    comment = "#>",
+    collapse = TRUE,
+    fig.show = "hold"
+    # tidy = TRUE,
+    # tidy.opts=list(width.cutoff=60)
+  )
+}
+
+
 
 options(
   dplyr.print_min = 6,
